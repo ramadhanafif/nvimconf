a029029609e002dfda96fe44f94108be4fa90c6e
Afif Ramadhan <ramadhanafif@gmail.com>
2023-05-24 07:29:37 +0700

    add transparent vim

--- init.lua
+++ init.lua
@@ -19,8 +19,8 @@ return {
   -- Set colorscheme to use
   -- colorscheme = "catppuccin",
   colorscheme = "everforest",
-  -- Load neovide.lua
-  -- require("neovide"),
+
+  
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,22 +1,27 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-{
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-}
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+
+    {
+        "xiyaowong/transparent.nvim",
+        lazy = false
+    },
 }
